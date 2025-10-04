20bb1b6a9cd8a6b58f269c50a03e58026f6afb05
astha56 <Astha.Basnet@mail.bcu.ac.uk>
2025-10-04 16:25:26 +0545

    Removed secret key from settings.py and added .env to .gitignore

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv( )
 
 
 
@@ -162,6 +165,7 @@ APPEND_SLASH = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "sk_test_51RyrJh3BS8BoCmLZUhMaHzFVHcxzs4E08ZB5yFXuac8rQKq4AOePxjm2JSk1XrasJL5uzjRyCjwADplMh9nwlZli00WVKR478l")
 STRIPE_PUBLISHED_KEY = os.getenv ("STRIPE_PUBLISHED_KEY", "pk_test_51RyrJh3BS8BoCmLZ7iOr6wroTJlquReEKwBnDC6wq8tHIkQC1XrLukoHdouYP2anMsS1mhLcFghXqMHPq8FkdUvx00VC2S41Ds")
