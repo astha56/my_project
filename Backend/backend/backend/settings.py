1922096a66ede5008300dee3d7bad72c195ac7ae
astha56 <Astha.Basnet@mail.bcu.ac.uk>
2025-12-11 10:20:26 +0545

    Configured environment variables for Stripe

--- Backend/backend/backend/settings.py
+++ Backend/backend/backend/settings.py
@@ -12,14 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -164,8 +161,5 @@ APPEND_SLASH = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
-STRIPE_PUBLISHED_KEY = os.getenv("STRIPE_PUBLISHED_KEY")
 
 
